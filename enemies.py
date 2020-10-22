c298ab95e8e5c8701e820453e8e502f780052996
Will <shepherdwl@cardiff.ac.uk>
2020-10-22 17:37:52 +0100

    Regular Enemies

---
+++ enemies.py
@@ -0,0 +1,41 @@
+class Werewolf(object)
+    name = "Werewolf"
+    health = 30
+    strength = 10
+    magic = 0
+    description = ("Physcially powerful and hard to kill")
+
+class Gnome(object)
+    name = "Gnome"
+    health = 40
+    strength = 10
+    magic = 4
+    escription = ("Gnomes are the smallest form of humanoids")
+    
+class SwampDragon(object)
+    name = "Swamp Dragon"
+    health = 55
+    strength = 13
+    magic = 10
+    description = ("Will eat and drink anything that can be used as fuel for fire-breathing")
+
+class Basillisk(object)
+    name = "Basillisk"
+    health = 80
+    strength = 25
+    magic = 16
+    description = ("Powerful and giant snake")
+
+class Chimera(object)
+    name = "Chimera"
+    health = 70
+    strength = 18
+    magic = 16
+    description = ("Native to Klatch, incredibly rare species")
+
+class ShadowingLemma(object)
+    name = "Shadowing Lemma"
+    health = 55
+    strength = 19
+    magic = 25
+    description = ("A curious creature that exists in only two dimensions, and eats mathematicians.")

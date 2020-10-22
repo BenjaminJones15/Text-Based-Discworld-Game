d3155f61a159ab18aa24ad151a2937ffa9cef5d1
Will <shepherdwl@cardiff.ac.uk>
2020-10-22 17:36:14 +0100

    Bosses

---
+++ boss.py
@@ -0,0 +1,40 @@
+class Johnathan(object):
+    name = "Johnathan"
+    health = 100
+    strength = 5
+    magic = 0
+    description = ("Most hated in Ankh-Morpork")
+
+class CarcerDun(object):
+    name = "Carcer Dun"
+    health = 150
+    strength = 30
+    magic = 20
+    description = ("Stone-cold killer")
+    
+class Gitlab(object):
+    name = "Gitlab"
+    health = 250
+    strength = 45
+    magic = 30
+    description = ("Why do we even use this?")
+    
+class MrGryle(object):
+    name = "Mr Gryle"
+    health = 300
+    strength = 55
+    magic = 40
+    description = ("Powerful Banshee")
+
+class Kirill(object):
+    name = "Kirill"
+    health = 400
+    strength = 60
+    magic = 60
+    description = ("Biggest badass in Ankh-Morpork")
+
+
+
+
+    
+

65265f8a42e47a4029bed59483ed06ffa83f1f40
Benjamin Jones <jonesbg3@cardiff.ac.uk>
2020-10-24 15:32:03 +0100

    Still working on random encounters.

--- Main.py
+++ Main.py
@@ -3,15 +3,25 @@ from combat import *
 from enemies import *
 from Map import *
 from normalise import *
+import random
 
 CurrentLocation = Pseudopolis_Yard_Inside
 
 def main():
-    
+    print()
 
     #commenting
 
 def RndEncounter(location):
-    if location.name = "The Library":
-        
+    if location.name == "The Library":
+        print_battle(ShadowingLemma)
+    elif location.name == "The Shades":
+        print_battle(Mugger)
+    else:
+        EnChance = random.randint(0,1)
+        if EnChance == 0:
+            return()
+        else:
+            print_battle()
+
 

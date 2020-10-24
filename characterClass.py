526e1906674d951260554ea47f8f105d4c5bca26
Jamie Groom <groomj@cardiff.ac.uk>
2020-10-24 15:06:47 +0100

    Character class dictionary

---
+++ characterClass.py
@@ -0,0 +1,35 @@
+class_wizard = {
+    # Low phys dmg, high mag dmg, med hp, high mana, 2 mana potions
+    "name": "Wizard",
+    "health": 100,
+    "mana": 100,
+    "strength": 15,
+    "description": "The wizard  deals high magic damage, with a high amount of mana. Beware, as the wizard has a lower amount of health.\n Starts with 2 mana pies"
+    # "magicDmg": 30,
+}
+
+class_warrior = {
+    # Med phys dmg, low mag dmg, med hp, med mana, 1 mana 1 hp potions
+    "name": "Warrior",
+    "health": 130,
+    "mana": 70,
+    "strength": 12,
+    "description": "The warrior deals medium amounts of damage, and has a fairly high amount of health.\n Starts with 1 mana and 1 health pie."
+    # "magicDmg": 5,
+}
+
+class_undead = {
+    # Med dmg, high hp, low mana, 2 strength potions (when added)
+    "name": "Undead",
+    "health": 150,
+    "mana": 50,
+    "strength": 10,
+    "description": "The undead compensates for his smaller mana pool for a very amount high of health.\n Starts with 2 strength pies."
+    # "magicDmg": 10,
+}
+
+Classes = {
+    "Wizard": class_wizard,
+    "Warrior": class_warrior,
+    "Undead": class_undead
+}

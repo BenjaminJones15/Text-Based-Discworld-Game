4bae63089e8b1cadfb7e42efb9d4e68ae46e00f7
Benjamin Jones <jonesbg3@cardiff.ac.uk>
2020-10-25 23:44:20 +0000

    Added in CurEnemy var which is seen in combat.

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from Map import *
 from Player import *
 import random
 
-CurrentLocation = Pseudopolis_Yard_Inside
+CurrentLocation = Pseudopolis_Yard_Inside       #would need to move to combat.py if also moving save and load 
 saveHealth = 0
 saveMana = 0
 saveExp = 0
@@ -12,22 +12,27 @@ saveInventory = ""
 saveLocation = ""
 
 def main():
-    print()
+    StartMenu()
+    
  
     #commenting
 
 def RndEncounter(location):
     if location.name == "The Library":
-        print_battle(ShadowingLemma)
+        CurEnemy = "ShadowingLemma"
+        start_battle()
     elif location.name == "The Shades":
-        print_battle(Mugger)
+        CurEnemy = "Mugger"
+        start_battle()
     else:
         EnChance = random.randint(0,1)
         if EnChance == 0:
             return()
         else:
             CurEnemy = EnemyList[random.randint(0,len(EnemyList)-1)]    #selects an enemy from the list of sprites
-            print_battle(CurEnemy)
+            start_battle()
+
+#to solve errors need to move both functions into combat.py, and then call them from here.
 
 
 def save_checkpoint():  # saves all the players stats after reaching a checkpoint

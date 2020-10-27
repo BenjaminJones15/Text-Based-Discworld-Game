020f1c9d91033c9988e7b601aad91b560a6f433a
Benjamin Jones <jonesbg3@cardiff.ac.uk>
2020-10-27 20:12:56 +0000

    Fixed store

--- store.py
+++ store.py
@@ -32,26 +32,20 @@ def store():
 
     if optioncmot == "1":
         print("You have bought a health pie for 5 AM$")
-        wallet = wallet - 5
-        for items in inventory:
-            Health_Pie = inventory["health pie"]
-            inventory.append(Health_Pie)
+        wallet = wallet - 5        
+        inventory["health pie"] += 1
 
     if optioncmot == "2":
         print("You have bought a mana pie for 20 AM$")
         wallet = wallet - 20
-        for items in inventory:
-            Mana_Pie = inventory["mana pie"]
-            inventory.append(Mana_Pie)
+        inventory["mana pie"] += 1
 
     if optioncmot == "3":
         print("You have bought a strength pie for 12 AM$")
         wallet = wallet - 12
-        for items in inventory:
-            Strength_Pie = inventory["strength pie"]
-            inventory.append(Strength_Pie)
+        inventory["strength pie"] += 1
 
-    if optioncmot == "X":
+    if optioncmot.lower == "x":
         store = False
 
     if optioncmot not in options:

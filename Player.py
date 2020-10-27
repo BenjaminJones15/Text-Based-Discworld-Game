cda0eebefe783f120136bdee65e3647736809113
Benjamin Jones <jonesbg3@cardiff.ac.uk>
2020-10-27 12:12:09 +0000

    Fixed it.

--- Player.py
+++ Player.py
@@ -54,8 +54,6 @@ def starting_inventory(player):  # Gives the player starting items depending on 
 
     return player.inventory  # Return updated player inventory
 
-player = Player("","")
-
 def StartMenu():  # Where the program begins. Can change when everything is implemented
     global player
     print("Enter your name")
